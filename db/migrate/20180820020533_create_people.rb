c95a17ef0b661604f2b5eab8c4fc7ca09f92230c
jeyce987 <jeyce_jp@hotmail.com>
2018-08-19 23:35:35 -0300

    criando primeiro scafolld

---
+++ db/migrate/20180820020533_create_people.rb
@@ -0,0 +1,13 @@
+class CreatePeople < ActiveRecord::Migration[5.1]
+  def change
+    create_table :people do |t|
+      t.string :name, limit: 50, null:false
+      t.string :email, limit: 100, index: {unique: true}
+      t.string :password, limit: 100
+      t.date :born_at
+      t.boolean :admin
+
+      t.timestamps null: false
+    end
+  end
+end
